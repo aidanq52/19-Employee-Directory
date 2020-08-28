3123a013a418a8b64ed0e82cb3218170c1ae0f8d
Aidan Q <aidanq52@gmail.com>
2020-08-27 20:13:54 -0700

    adding search to state and a method to update search

---
+++ src/components/SearchForm/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Form, Button} from "react-bootstrap";
+
+function SearchForm(props){
+    return (
+        <Form>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Search by Name</Form.Label>
+                <Form.Control type="email" placeholder="Enter name" onChange={ (e)=> props.updateSearch(e.target.value)}/>
+            </Form.Group>
+
+            <Button variant="primary" type="submit" onClick={e => e.preventDefault()}>
+                    Search
+            </Button>
+
+        </Form>
+    )
+}
+
+export default SearchForm;
--- src/components/UserTable/index.js
+++ src/components/UserTable/index.js
@@ -3,14 +3,32 @@ import {Table} from "react-bootstrap";
 
 function UserTable(props){
 
+    // props.users.filter();
 
     return(
         <Table striped bordered hover>
-            <th scope="col">First Name</th>
-            <th scope="col">Last Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Phone #</th>
-            <th scope="col">Address</th>
+            <thead>
+                <tr>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone #</th>
+                    <th scope="col">Location</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.users.map(user =>{
+                    return (
+                        <tr key={user.id.value}>
+                            <td>{user.name.first}</td>
+                            <td>{user.name.last}</td>
+                            <td>{user.email}</td>
+                            <td>{user.phone}}</td>
+                            <td>{user.location.city}, {user.location.state}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
         </Table>
     )
 }
